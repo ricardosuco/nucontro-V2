3fca4dd892441473fd85e865038e0a9b6921793c
Ricardo Avelino <rick58@ymail.com>
2024-12-29 22:32:59 -0300

    test: verificação de funcionamento do teste runner

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('Hello, World!')
+</script>
+
+<style scoped></style>
