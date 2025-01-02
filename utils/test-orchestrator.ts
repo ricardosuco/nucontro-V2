b70ee332a895130be8f30194ce5139455c3da3f7
Ricardo Avelino <rick58@ymail.com>
2025-01-02 11:22:05 -0300

    feat: ajusta env `url` em schema

--- utils/test-orchestrator.ts
+++ utils/test-orchestrator.ts
@@ -5,7 +5,7 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
-      maxTimeout: 1000,
+      maxTimeout: 2000,
     })
 
     async function fetchStatusPage() {
